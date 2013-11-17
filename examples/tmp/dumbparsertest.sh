f03a44fd8a70dd432186a82a35ce957956ac542d
Chris D'Angelo <cd2665@columbia.edu>
2013-11-16 19:08:34 -0500

    parsing of complex tree assignment solved (still testing)

---
+++ examples/tmp/dumbparsertest.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+../../lorax -a < helloworld_sprint.lrx
+../../lorax -a < functest.lrx
+../../lorax -a < ../dfs.lrx
+../../lorax -a < ../gcd.lrx
+../../lorax -a < ../helloworld.lrx
+../../lorax -a < experimental.lrx
+../../lorax -a < parserkitchensink.lrx
+../../lorax -a < complex_tree_assignments.lrx
