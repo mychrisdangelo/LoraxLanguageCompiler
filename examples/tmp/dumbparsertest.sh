78bab06980883a60c0d4765579dd4633e35c0ac4
Chris D'Angelo <cd2665@columbia.edu>
2013-11-16 22:22:02 -0500

    parser tests in testsuite. hello world added to fail

--- examples/tmp/dumbparsertest.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-../../lorax -a < helloworld_sprint.lrx
-../../lorax -a < functest.lrx
-../../lorax -a < ../dfs.lrx
-../../lorax -a < ../gcd.lrx
-../../lorax -a < ../helloworld.lrx
-../../lorax -a < parserkitchensink.lrx
-../../lorax -a < complex_tree_assignments.lrx
