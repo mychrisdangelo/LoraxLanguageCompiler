f0f86edbca71b443032db15d26f65fa9e19173d3
Tim Paine <t.paine154@gmail.com>
2013-11-20 15:53:24 -0500

    More symtab comments

--- ssymtab.ml
+++ ssymtab.ml
@@ -5,20 +5,32 @@
 
 open Ast
 
+(* Maps a current environment to the scope of the block
+ * in which it is contained
+ *)
 module SymMap = Map.Make(String)
+
+
+(*this holds the activation record data*)
 let scope_parents = Array.create 1000 0
 
-(*
-let block_id = ref 1
 
+
+(*
+type block = {
+        locals: var list;
+        statements: stmt list;
+        block_id: int;
+}
+let block_id = ref 1
 let gen_block_id (u:unit) =
-	let x = block_id.contents in
-		block_id := x + 1; x
+    let x = block_id.contents in
+    block_id := x + 1; x
 *)
 
 
-
 (*Print the symbol table of the given environment*)
+(*Don't need to change*)
 let string_of_symtab env =
 	let symlist = SymMap.fold
 		(fun s t prefix -> (string_of_decl t) :: prefix) (fst env) [] in
@@ -52,10 +64,11 @@ let rec symtab_add_vars (vars:var list) env =
 (* add declarations inside statements to the symbol table *)
 let rec symtab_add_stmts (stmts:stmt list) env =
 	match stmts with
-	[] -> env
+	[] -> env (*block contains no statements*)
 	| head :: tail -> let env = (match head with
-		Block(s) -> symtab_add_block s env
-		| For(e1,e2,e3,s) -> symtab_add_block s env
+		Block(s) -> symtab_add_block s env (*statement is an arbitrary block*)
+		| For(e1,e2,e3,s) -> symtab_add_block s env (*add the for's block to the
+        record*)
 		| While(e, s) -> symtab_add_block s env
 		| If(e, s1, s2) -> let env = symtab_add_block s1 env in symtab_add_block s2 env 
 		| _ -> env) in symtab_add_stmts tail env
@@ -63,23 +76,31 @@ let rec symtab_add_stmts (stmts:stmt list) env =
 
 (* need to check this *)
 and symtab_add_block (b:block) env =
-	if(b.block_id != -1) then
-		let (tab, scope) = env in
-		let env = symtab_add_vars b.locals (tab, b.block_id) in
+	if(b.block_id != -1) then 
+		let (table, scope) = env in (*get current environment*)
+		let env = symtab_add_vars b.locals (table, b.block_id) in
+        (*add the block's local variables to the table with scope
+         * equal to the current block's id*)
 		let env = symtab_add_stmts b.statements env in
+        (*add all statements, need to do all subblocks
+         * before we do the outer block*)
 		scope_parents.(b.block_id) <- scope; ((fst env), scope)
+        (*add the current block to the parent scope table
+         * i.e. the parent scope of this block is equal to 
+         * the current scope of the environment*)
 	else env
 
 and symtab_add_func (f:func) env =
-	let scope = snd env in
-	let args = List.map snd f.formals in
-	let env = symtab_add_decl f.name (FuncDecl(f.name, f.ret_type, args, scope)) env in
-	let env = symtab_add_vars f.formals ((fst env), f.body.block_id) in
-	symtab_add_block f.body ((fst env), scope)
+	let scope = snd env in (*current scope is 2nd element in env*)
+	let args = List.map snd f.formals in (*gets name of every formal*)
+	let env = symtab_add_decl f.name (FuncDecl(f.name, f.ret_type, args, scope)) env in 
+    (*add current function to table*)
+	let env = symtab_add_vars f.formals ((fst env), f.body.block_id) in 
+    (*adds vars to table*)
+	symtab_add_block f.body ((fst env), scope) (*add body to symtable given
+    current environment and scope*) 
 
 
-
-(*Below here should not change much *)
 (* add list of functions to the symbol table *)
 and symtab_add_funcs (funcs:func list) env =
 	match funcs with
@@ -88,7 +109,6 @@ and symtab_add_funcs (funcs:func list) env =
 		symtab_add_funcs tail env
 
 
-(* add other built ins and types*)
 (* add builtin functions to the symbol table *)
 let add_builtins env =
 	let env = symtab_add_decl "print" (FuncDecl("print", Lrx_Atom(Null), [Simple(Str)], 0)) env (*in
