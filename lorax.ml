06e4aec12b957bd742b69a22d5bd01d2c8d238cb
Chris D'Angelo <cd2665@columbia.edu>
2013-11-23 18:28:57 -0500

    added symtab output for lorax.ml

--- lorax.ml
+++ lorax.ml
@@ -1,16 +1,19 @@
-type action = Ast | Compile | Binary
+type action = Ast | Compile | Binary | Symtab
 
 let _ =
   let action = if Array.length Sys.argv > 1 then
     List.assoc Sys.argv.(1) [ ("-a", Ast);
 			      ("-c", Compile);
-			      ("-b", Binary) ]
+			      ("-b", Binary);
+            ("-s", Symtab) ]
   else Compile in
   let lexbuf = Lexing.from_channel stdin in
   let program = Parser.program Scanner.token lexbuf in
   match action with
       Ast        -> let listing = Ast.string_of_program program
                     in print_string listing
+    | Symtab    -> let env = Symtab.symtab_of_program program in
+                    print_string (Symtab.string_of_symtab env)
     | Compile    -> let env = Symtab.symtab_of_program program in
                     print_string (Symtab.string_of_symtab env)
     | Binary     -> raise (Failure "binary not implemented")
