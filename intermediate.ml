94a12494afe5d462f1758060f532ae9c58b9a0ab
Kira Whitehouse <kbw2116@columbia.edu>
2013-11-28 23:41:55 -0500

    trying to understand intermediate.ml; sketched out our versions of ir_variables but not sure about some of the return types

--- intermediate.ml
+++ intermediate.ml
@@ -1,57 +1,53 @@
 open Ast
 open Check
 
-(*
+
 type inter_var = Ast.var
 type inter_var_type = Ast.var_type
-type inter_fdecl = Ast.func_decl
-
-
-type inter_lit = 
-  IntLit of int
-  | FloatLit of float
-  | StringLit of string
-  | CharLit of char
-  | BoolLit of bool
-  | NullLit
+type inter_func_decl = Ast.scope_func_decl
 
 type inter_expr =
- Binop of inter_var *  inter_var * Ast.op * inter_var
- | Unop of inter_var * inter_var * Ast.uop
- | Call of inter_var * inter_fdecl * inter_var list
- | Lit of inter_var * inter_lit
- | Tree of inter_var * inter_expr * inter_expr list
- (* We need to talk about the following two cases as a group/I dont really know
-  * how this should be handled *)
- | Assign of inter_var * inter_expr * inter_expr (* this is most likely
- incorrect *)
+    Ir_Int_Literal of int
+  | Ir_Float_Literal of float
+  | Ir_String_Literal of string
+  | Ir_Char_Literal of char
+  | Ir_Bool_Literal of bool
+  | Ir_Null_Literal
+  | Ir_Id of inter_var_type * string
+  | Ir_Binop of inter_var_type * inter_expr * op * inter_expr
+  | Ir_Unop of inter_var_type * inter_expr * uop
+  | Ir_Tree of inter_var_type * int * inter_expr * inter_expr list
+  | Ir_Assign of inter_var_type * inter_expr * inter_expr
+  | Ir_Call of inter_func_decl * inter_expr list
+  | Ir_Noexpr
 
 type inter_stmt =
-  Decl of inter_var
-  | If of inter_var * string (* temporary *)
-  | Jmp of string
-  | Label of string
-  | Ret of inter_var
-  | Expr of inter_expr
+    Ir_CodeBlock of inter_block
+  | Ir_Expr of inter_expr
+  | Ir_Return of inter_expr
+  | Ir_If of inter_expr * inter_block * inter_block
+  | Ir_For of inter_expr * inter_expr * inter_expr * inter_block
+  | Ir_While of inter_expr * inter_block
+  | Ir_Continue
+  | Ir_Break
+
+
+and inter_block = {
+    ir_locals: inter_var list;
+    ir_statements: inter_stmt list;
+    ir_block_id: int;
+}
 
 and inter_func = {
-  args: inter_var list;
-  header: inter_fdecl;
-  code: inter_stmt list;
+  ir_name: string;
+  ir_ret_type: inter_var_type;
+  ir_formals: inter_var list;
+  ir_block: inter_block;
 }
 
-and inter_prototype = {
-  name: string;
-  ret_type: inter_var_type;
-  formals: inter_var list;
-}
+type inter_pgrm = inter_var list * inter_func list
 
-type inter_pgrm = {
-  globals: inter_var list;
-  fdecls: inter_fdecl list;
-  funcs: inter_func list;
-}
-
+(*
 
 
 and simplify_assign (v:var_type) (e1:c_expr) (e2:c_expr) = function
@@ -146,12 +142,63 @@ if NoExpr:
 *)
 *)
 
-(* 
- * To: Doug/Zhaarn/Tim
- * From: Chris
- * Message: this is the entry point function that I'm using in 
- * lorax.ml. If you can please keep it this way. 
- *)
+(* let gen_ir_id x (id:int) =
+  let prefix = 
+    match x with
+      Lrx_Tree(t, d) -> ("__ir_tree_" ^ string_of_var_type t ^ "_" ^ string_of_int d ^ "_")
+      | Lrx_Atom(Lrx_Bool) -> "__ir_bool_" 
+      | Lrx_Atom(Lrx_Char) -> "__ir_char_"
+      | Lrx_Atom(Lrx_Int) -> "__ir_int_"
+      | Lrx_Atom(Lrx_Float) -> "__ir_float_"
+      | _ -> raise(Failure("attempting to generate ir id for unsupported type"))) 
+  in (prefix ^ (string_of_int id), x, -1, id + 1)
+
+let rec gen_ir_expr (e:c_expr) (id:int) =
+  match e with
+       C_Int_Literal(i) -> 
+       let (s, v, _, id) = gen_ir_id Lrx_Atom(Lrx_Int) id in
+       ([Decl((s,v,_,id))); Expr(Lit((s,v,_,id), IntLit(i)))], (s,v,_,id)) //
+     | C_Float_Literal(f) -> 
+     | C_String_Literal(s) ->
+     | C_Char_Literal(c) -> 
+     | C_Bool_Literal(b) -> 
+     | C_Tree(t, d, e, el) ->
+     | C_Id(t, s) -> 
+     | C_Binop(t, e1, op, e2) ->
+     | C_Assign(t, l, r) ->
+     | C_Unop(t, e, op) ->
+     | C_Call(fd, el) -> 
+     | C_Null_Literal ->
+     | C_Noexpr -> 
+
+let rec gen_ir_statement (s:c_stmt) =
+    match s with
+       C_CodeBlock(b) ->
+     | C_Return(e) ->
+     | C_Expr(e) -> 
+     | C_If(e, b1, b2) -> 
+     | C_For(e1, e2, e3, b) -> 
+     | C_While(e, b) -> 
+     | C_Continue ->
+     | C_Break -> *)
+
+let rec gen_ir_func (f:c_func) =
+  ()
+(*let ir_block = gen_ir_block f.c_fblock in 
+  let ir_formals = gen_ir_expr f.c_formals in
+  {f.c_fname, f.c_ret_type, ir_formals, ir_block} *)
+
+and gen_ir_funcs (flist:c_func list) =
+  match flist with
+  [] -> []
+  | head :: tail -> gen_ir_func head :: gen_ir_funcs tail
+
+and gen_ir_decls (flist:var list) = 
+  match flist with
+  [] -> []
+  | head :: tail -> (fst head) :: gen_ir_decls tail
 
 let rec intermediate_rep_program (p:c_program) =
-  "dummy return value"
+  let ir_decls = gen_ir_decls (fst p) in 
+  let ir_funcs = gen_ir_funcs (snd p) in 
+  (ir_decls, ir_funcs)
