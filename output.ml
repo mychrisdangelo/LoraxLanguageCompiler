6010b8a89d9e06b6ced6250c1576eb831a86fb8e
Kira Whitehouse <kbw2116@columbia.edu>
2013-12-01 23:43:52 -0500

    fixed var decls and default return type; added test; started statement implementation in output and ir

--- output.ml
+++ output.ml
@@ -30,7 +30,7 @@ let c_of_var_type = function
 	| Lrx_Atom(Lrx_Bool) -> "int"
 	| Lrx_Atom(Lrx_Char) -> "char"
 (* 	| Lrx_Tree -> "struct lrx_tree"
- *)	| _ -> raise(Failure("TEMP"))
+ *)	| _ -> raise(Failure("TEMP C of VAR TYPE"))
 
 let c_of_var_def (v:scope_var_decl) = 
 	let (_,t, _) = v in match t with
@@ -38,10 +38,11 @@ let c_of_var_def (v:scope_var_decl) =
 	| Lrx_Atom(Lrx_Float) -> "0.0"
 	| Lrx_Atom(Lrx_Bool) -> "false"
 	| Lrx_Atom(Lrx_Char) -> "\'0\'"
+	| _ -> raise(Failure ("TEMP C of VAR DECL"))
 
 let c_of_var_decl (v:scope_var_decl) =
 	let (n,t,s) = v in 
-	 c_of_var_type t ^ " " ^ n ^ string_of_int s
+	 c_of_var_type t ^ " " ^ n ^ "_" ^ string_of_int s
 
 let c_of_var_decl_list = function
 	[] -> "" 
@@ -51,10 +52,24 @@ let c_of_func_decl_args = function
 	[] -> ""
 	| args -> String.concat (", ") (List.map c_of_var_decl args)
 
+let c_of_var_name (v:scope_var_decl) = 
+	let (n,_,s) = v in 
+	 n ^ "_" ^ string_of_int s
+
+let c_of_expr = function
+  	Ir_Int_Literal(v, i) -> c_of_var_name v ^ " = " ^ string_of_int i
+  	| Ir_Float_Literal(v, f) ->  c_of_var_name v ^ " = " ^ string_of_float f
+  	| Ir_String_Literal(v, s) -> c_of_var_name v ^ " = " ^ s
+  	| Ir_Char_Literal(v, c) -> c_of_var_name v ^ " = " ^ "\'" ^ String.make 1 c ^ "\'"
+  	| Ir_Bool_Literal(v, b) -> c_of_var_name v ^ " = " ^ string_of_bool b
+  	| _ -> (raise (Failure ("TEMP C of EXPR")))
+
 let c_of_stmt (v:ir_stmt) =
 	match v with 
 	 Ir_Decl(d) -> c_of_var_decl d ^ " = " ^ c_of_var_def d
-  	| Ir_Ret(n, t, s) -> "return " ^ n ^ string_of_int s
+  	| Ir_Ret(v) -> "return " ^ c_of_var_name v;
+   	| Ir_Expr(e) -> c_of_expr e
+    | _ -> raise (Failure ("TMP C of STMT"))
 
 let c_of_stmt_list = function
 	[] -> ""
@@ -63,7 +78,7 @@ let c_of_stmt_list = function
 let c_of_func (f: ir_func) =
 	let (t, n, sl) = f.ir_header in 
 	c_of_var_type t ^ " " ^ n ^ "(" ^ c_of_func_decl_args sl ^ ")\n{\n" ^
-	c_of_var_decl_list f.ir_vdecls ^ c_of_stmt_list f.ir_stmts ^ "}"
+	c_of_stmt_list f.ir_vdecls ^ c_of_stmt_list f.ir_stmts ^ "}"
 
 let c_of_func_list = function
 	[] -> "" 
