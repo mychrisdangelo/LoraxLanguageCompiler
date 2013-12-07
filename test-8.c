bc49f53ee05d108345fe30e19690b58ac80ebca2
Chris D'Angelo <cd2665@columbia.edu>
2013-12-07 16:01:52 -0500

    added test-8 to indicate ordering of tree nodes needs work

---
+++ test-8.c
@@ -0,0 +1,298 @@
+#include "lrxlib.h"
+int main();
+
+int main()
+{
+tree * t_1 = lrx_declare_tree(_CHAR_, 2);
+tree * t2_1 = lrx_declare_tree(_INT_, 3);
+tree * t3_1 = lrx_declare_tree(_FLOAT_, 3);
+tree * t4_1 = lrx_declare_tree(_BOOL_, 2);
+tree * __tmp_tree_datatype_char_degree_2_92 = lrx_declare_tree(_CHAR_, 2);
+tree * __tmp_tree_datatype_char_degree_2_82 = lrx_declare_tree(_CHAR_, 2);
+char __tmp_char_80 = '\0';
+char __tmp_char_81 = '\0';
+char __tmp_char_79 = '\0';
+tree * __tmp_tree_datatype_char_degree_2_85 = lrx_declare_tree(_CHAR_, 2);
+tree * __tmp_tree_datatype_char_degree_2_88 = lrx_declare_tree(_CHAR_, 2);
+char __tmp_char_91 = '\0';
+char __tmp_char_78 = '\0';
+tree * __tmp_tree_datatype_char_degree_2_95 = lrx_declare_tree(_CHAR_, 2);
+tree * __tmp_tree_datatype_int_degree_3_69 = lrx_declare_tree(_INT_, 3);
+int __tmp_int_51 = 0;
+tree * __tmp_tree_datatype_int_degree_3_56 = lrx_declare_tree(_INT_, 3);
+int __tmp_int_53 = 0;
+int __tmp_int_54 = 0;
+int __tmp_int_55 = 0;
+int __tmp_int_52 = 0;
+tree * __tmp_tree_datatype_int_degree_3_59 = lrx_declare_tree(_INT_, 3);
+tree * __tmp_tree_datatype_int_degree_3_62 = lrx_declare_tree(_INT_, 3);
+tree * __tmp_tree_datatype_int_degree_3_65 = lrx_declare_tree(_INT_, 3);
+int __tmp_int_68 = 0;
+int __tmp_int_50 = 0;
+tree * __tmp_tree_datatype_int_degree_3_72 = lrx_declare_tree(_INT_, 3);
+tree * __tmp_tree_datatype_int_degree_3_75 = lrx_declare_tree(_INT_, 3);
+tree * __tmp_tree_datatype_float_degree_3_41 = lrx_declare_tree(_FLOAT_, 3);
+float __tmp_float_23 = 0.0;
+float __tmp_float_24 = 0.0;
+tree * __tmp_tree_datatype_float_degree_3_29 = lrx_declare_tree(_FLOAT_, 3);
+float __tmp_float_26 = 0.0;
+float __tmp_float_27 = 0.0;
+float __tmp_float_28 = 0.0;
+float __tmp_float_25 = 0.0;
+tree * __tmp_tree_datatype_float_degree_3_32 = lrx_declare_tree(_FLOAT_, 3);
+tree * __tmp_tree_datatype_float_degree_3_35 = lrx_declare_tree(_FLOAT_, 3);
+tree * __tmp_tree_datatype_float_degree_3_38 = lrx_declare_tree(_FLOAT_, 3);
+float __tmp_float_22 = 0.0;
+tree * __tmp_tree_datatype_float_degree_3_44 = lrx_declare_tree(_FLOAT_, 3);
+tree * __tmp_tree_datatype_float_degree_3_47 = lrx_declare_tree(_FLOAT_, 3);
+tree * __tmp_tree_datatype_bool_degree_2_16 = lrx_declare_tree(_BOOL_, 2);
+tree * __tmp_tree_datatype_bool_degree_2_6 = lrx_declare_tree(_BOOL_, 2);
+bool __tmp_bool_4 = false;
+bool __tmp_bool_5 = false;
+bool __tmp_bool_3 = false;
+tree * __tmp_tree_datatype_bool_degree_2_9 = lrx_declare_tree(_BOOL_, 2);
+tree * __tmp_tree_datatype_bool_degree_2_12 = lrx_declare_tree(_BOOL_, 2);
+bool __tmp_bool_15 = false;
+bool __tmp_bool_2 = false;
+tree * __tmp_tree_datatype_bool_degree_2_19 = lrx_declare_tree(_BOOL_, 2);
+int __tmp_int_1 = 0;
+int __tmp_int_0 = 0;
+
+__tmp_char_80 = 'j';
+
+__tmp_char_81 = 'o';
+
+__tmp_char_79 = 'i';
+
+char *__tmp_char_83 = &__tmp_char_81;
+tree * __tmp_tree_datatype_char_degree_2_84[2];
+__tmp_tree_datatype_char_degree_2_84[1] = NULL;
+__tmp_tree_datatype_char_degree_2_84[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_char_degree_2_85, __tmp_char_83, __tmp_tree_datatype_char_degree_2_84);
+
+char *__tmp_char_86 = &__tmp_char_80;
+tree * __tmp_tree_datatype_char_degree_2_87[2];
+__tmp_tree_datatype_char_degree_2_87[1] = NULL;
+__tmp_tree_datatype_char_degree_2_87[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_char_degree_2_88, __tmp_char_86, __tmp_tree_datatype_char_degree_2_87);
+
+tree * __tmp_tree_datatype_char_degree_2_89[2];
+__tmp_tree_datatype_char_degree_2_89[0] = __tmp_tree_datatype_char_degree_2_85;
+__tmp_tree_datatype_char_degree_2_89[1] = __tmp_tree_datatype_char_degree_2_88;
+char *__tmp_char_90 = &__tmp_char_79;
+lrx_define_tree(__tmp_tree_datatype_char_degree_2_82, __tmp_char_90, __tmp_tree_datatype_char_degree_2_89);
+
+__tmp_char_91 = 'k';
+
+__tmp_char_78 = 'h';
+
+char *__tmp_char_93 = &__tmp_char_91;
+tree * __tmp_tree_datatype_char_degree_2_94[2];
+__tmp_tree_datatype_char_degree_2_94[1] = NULL;
+__tmp_tree_datatype_char_degree_2_94[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_char_degree_2_95, __tmp_char_93, __tmp_tree_datatype_char_degree_2_94);
+
+tree * __tmp_tree_datatype_char_degree_2_96[2];
+__tmp_tree_datatype_char_degree_2_96[0] = __tmp_tree_datatype_char_degree_2_82;
+__tmp_tree_datatype_char_degree_2_96[1] = __tmp_tree_datatype_char_degree_2_95;
+char *__tmp_char_97 = &__tmp_char_78;
+lrx_define_tree(__tmp_tree_datatype_char_degree_2_92, __tmp_char_97, __tmp_tree_datatype_char_degree_2_96);
+
+lrx_assign_tree_direct(&t_1, &__tmp_tree_datatype_char_degree_2_92);
+
+__tmp_int_51 = 2;
+
+__tmp_int_53 = 4;
+
+__tmp_int_54 = 5;
+
+__tmp_int_55 = 6;
+
+__tmp_int_52 = 3;
+
+int *__tmp_int_57 = &__tmp_int_55;
+tree * __tmp_tree_datatype_int_degree_3_58[3];
+__tmp_tree_datatype_int_degree_3_58[2] = NULL;
+__tmp_tree_datatype_int_degree_3_58[1] = NULL;
+__tmp_tree_datatype_int_degree_3_58[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_int_degree_3_59, __tmp_int_57, __tmp_tree_datatype_int_degree_3_58);
+
+int *__tmp_int_60 = &__tmp_int_54;
+tree * __tmp_tree_datatype_int_degree_3_61[3];
+__tmp_tree_datatype_int_degree_3_61[2] = NULL;
+__tmp_tree_datatype_int_degree_3_61[1] = NULL;
+__tmp_tree_datatype_int_degree_3_61[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_int_degree_3_62, __tmp_int_60, __tmp_tree_datatype_int_degree_3_61);
+
+int *__tmp_int_63 = &__tmp_int_53;
+tree * __tmp_tree_datatype_int_degree_3_64[3];
+__tmp_tree_datatype_int_degree_3_64[2] = NULL;
+__tmp_tree_datatype_int_degree_3_64[1] = NULL;
+__tmp_tree_datatype_int_degree_3_64[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_int_degree_3_65, __tmp_int_63, __tmp_tree_datatype_int_degree_3_64);
+
+tree * __tmp_tree_datatype_int_degree_3_66[3];
+__tmp_tree_datatype_int_degree_3_66[0] = __tmp_tree_datatype_int_degree_3_59;
+__tmp_tree_datatype_int_degree_3_66[1] = __tmp_tree_datatype_int_degree_3_62;
+__tmp_tree_datatype_int_degree_3_66[2] = __tmp_tree_datatype_int_degree_3_65;
+int *__tmp_int_67 = &__tmp_int_52;
+lrx_define_tree(__tmp_tree_datatype_int_degree_3_56, __tmp_int_67, __tmp_tree_datatype_int_degree_3_66);
+
+__tmp_int_68 = 7;
+
+__tmp_int_50 = 1;
+
+int *__tmp_int_70 = &__tmp_int_68;
+tree * __tmp_tree_datatype_int_degree_3_71[3];
+__tmp_tree_datatype_int_degree_3_71[2] = NULL;
+__tmp_tree_datatype_int_degree_3_71[1] = NULL;
+__tmp_tree_datatype_int_degree_3_71[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_int_degree_3_72, __tmp_int_70, __tmp_tree_datatype_int_degree_3_71);
+
+int *__tmp_int_73 = &__tmp_int_51;
+tree * __tmp_tree_datatype_int_degree_3_74[3];
+__tmp_tree_datatype_int_degree_3_74[2] = NULL;
+__tmp_tree_datatype_int_degree_3_74[1] = NULL;
+__tmp_tree_datatype_int_degree_3_74[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_int_degree_3_75, __tmp_int_73, __tmp_tree_datatype_int_degree_3_74);
+
+tree * __tmp_tree_datatype_int_degree_3_76[3];
+__tmp_tree_datatype_int_degree_3_76[0] = __tmp_tree_datatype_int_degree_3_56;
+__tmp_tree_datatype_int_degree_3_76[1] = __tmp_tree_datatype_int_degree_3_72;
+__tmp_tree_datatype_int_degree_3_76[2] = __tmp_tree_datatype_int_degree_3_75;
+int *__tmp_int_77 = &__tmp_int_50;
+lrx_define_tree(__tmp_tree_datatype_int_degree_3_69, __tmp_int_77, __tmp_tree_datatype_int_degree_3_76);
+
+lrx_assign_tree_direct(&t2_1, &__tmp_tree_datatype_int_degree_3_69);
+
+__tmp_float_23 = 2.1;
+
+__tmp_float_24 = 3.1;
+
+__tmp_float_26 = 5.2;
+
+__tmp_float_27 = 5.3;
+
+__tmp_float_28 = 5.4;
+
+__tmp_float_25 = 4.1;
+
+float *__tmp_float_30 = &__tmp_float_28;
+tree * __tmp_tree_datatype_float_degree_3_31[3];
+__tmp_tree_datatype_float_degree_3_31[2] = NULL;
+__tmp_tree_datatype_float_degree_3_31[1] = NULL;
+__tmp_tree_datatype_float_degree_3_31[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_float_degree_3_32, __tmp_float_30, __tmp_tree_datatype_float_degree_3_31);
+
+float *__tmp_float_33 = &__tmp_float_27;
+tree * __tmp_tree_datatype_float_degree_3_34[3];
+__tmp_tree_datatype_float_degree_3_34[2] = NULL;
+__tmp_tree_datatype_float_degree_3_34[1] = NULL;
+__tmp_tree_datatype_float_degree_3_34[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_float_degree_3_35, __tmp_float_33, __tmp_tree_datatype_float_degree_3_34);
+
+float *__tmp_float_36 = &__tmp_float_26;
+tree * __tmp_tree_datatype_float_degree_3_37[3];
+__tmp_tree_datatype_float_degree_3_37[2] = NULL;
+__tmp_tree_datatype_float_degree_3_37[1] = NULL;
+__tmp_tree_datatype_float_degree_3_37[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_float_degree_3_38, __tmp_float_36, __tmp_tree_datatype_float_degree_3_37);
+
+tree * __tmp_tree_datatype_float_degree_3_39[3];
+__tmp_tree_datatype_float_degree_3_39[0] = __tmp_tree_datatype_float_degree_3_32;
+__tmp_tree_datatype_float_degree_3_39[1] = __tmp_tree_datatype_float_degree_3_35;
+__tmp_tree_datatype_float_degree_3_39[2] = __tmp_tree_datatype_float_degree_3_38;
+float *__tmp_float_40 = &__tmp_float_25;
+lrx_define_tree(__tmp_tree_datatype_float_degree_3_29, __tmp_float_40, __tmp_tree_datatype_float_degree_3_39);
+
+__tmp_float_22 = 1.;
+
+float *__tmp_float_42 = &__tmp_float_24;
+tree * __tmp_tree_datatype_float_degree_3_43[3];
+__tmp_tree_datatype_float_degree_3_43[2] = NULL;
+__tmp_tree_datatype_float_degree_3_43[1] = NULL;
+__tmp_tree_datatype_float_degree_3_43[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_float_degree_3_44, __tmp_float_42, __tmp_tree_datatype_float_degree_3_43);
+
+float *__tmp_float_45 = &__tmp_float_23;
+tree * __tmp_tree_datatype_float_degree_3_46[3];
+__tmp_tree_datatype_float_degree_3_46[2] = NULL;
+__tmp_tree_datatype_float_degree_3_46[1] = NULL;
+__tmp_tree_datatype_float_degree_3_46[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_float_degree_3_47, __tmp_float_45, __tmp_tree_datatype_float_degree_3_46);
+
+tree * __tmp_tree_datatype_float_degree_3_48[3];
+__tmp_tree_datatype_float_degree_3_48[0] = __tmp_tree_datatype_float_degree_3_29;
+__tmp_tree_datatype_float_degree_3_48[1] = __tmp_tree_datatype_float_degree_3_44;
+__tmp_tree_datatype_float_degree_3_48[2] = __tmp_tree_datatype_float_degree_3_47;
+float *__tmp_float_49 = &__tmp_float_22;
+lrx_define_tree(__tmp_tree_datatype_float_degree_3_41, __tmp_float_49, __tmp_tree_datatype_float_degree_3_48);
+
+lrx_assign_tree_direct(&t3_1, &__tmp_tree_datatype_float_degree_3_41);
+
+__tmp_bool_4 = true;
+
+__tmp_bool_5 = false;
+
+__tmp_bool_3 = false;
+
+bool *__tmp_bool_7 = &__tmp_bool_5;
+tree * __tmp_tree_datatype_bool_degree_2_8[2];
+__tmp_tree_datatype_bool_degree_2_8[1] = NULL;
+__tmp_tree_datatype_bool_degree_2_8[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_bool_degree_2_9, __tmp_bool_7, __tmp_tree_datatype_bool_degree_2_8);
+
+bool *__tmp_bool_10 = &__tmp_bool_4;
+tree * __tmp_tree_datatype_bool_degree_2_11[2];
+__tmp_tree_datatype_bool_degree_2_11[1] = NULL;
+__tmp_tree_datatype_bool_degree_2_11[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_bool_degree_2_12, __tmp_bool_10, __tmp_tree_datatype_bool_degree_2_11);
+
+tree * __tmp_tree_datatype_bool_degree_2_13[2];
+__tmp_tree_datatype_bool_degree_2_13[0] = __tmp_tree_datatype_bool_degree_2_9;
+__tmp_tree_datatype_bool_degree_2_13[1] = __tmp_tree_datatype_bool_degree_2_12;
+bool *__tmp_bool_14 = &__tmp_bool_3;
+lrx_define_tree(__tmp_tree_datatype_bool_degree_2_6, __tmp_bool_14, __tmp_tree_datatype_bool_degree_2_13);
+
+__tmp_bool_15 = true;
+
+__tmp_bool_2 = true;
+
+bool *__tmp_bool_17 = &__tmp_bool_15;
+tree * __tmp_tree_datatype_bool_degree_2_18[2];
+__tmp_tree_datatype_bool_degree_2_18[1] = NULL;
+__tmp_tree_datatype_bool_degree_2_18[0] = NULL;
+
+lrx_define_tree(__tmp_tree_datatype_bool_degree_2_19, __tmp_bool_17, __tmp_tree_datatype_bool_degree_2_18);
+
+tree * __tmp_tree_datatype_bool_degree_2_20[2];
+__tmp_tree_datatype_bool_degree_2_20[0] = __tmp_tree_datatype_bool_degree_2_6;
+__tmp_tree_datatype_bool_degree_2_20[1] = __tmp_tree_datatype_bool_degree_2_19;
+bool *__tmp_bool_21 = &__tmp_bool_2;
+lrx_define_tree(__tmp_tree_datatype_bool_degree_2_16, __tmp_bool_21, __tmp_tree_datatype_bool_degree_2_20);
+
+lrx_assign_tree_direct(&t4_1, &__tmp_tree_datatype_bool_degree_2_16);
+
+lrx_print_tree(t_1);
+lrx_print_tree(t2_1);
+lrx_print_tree(t3_1);
+lrx_print_tree(t4_1);
+
+return __tmp_int_0;
+
+}
