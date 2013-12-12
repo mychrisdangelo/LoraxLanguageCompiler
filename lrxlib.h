d565b3efde38a87de8cbd4660bff55763ec24116
Doug Bienstock <dmb2168@columbia.edu>
2013-12-12 00:21:12 -0500

    fixed increment error

--- lrxlib.h
+++ lrxlib.h
@@ -398,9 +398,9 @@ int _lrx_count_nodes( struct tree *t ) {
 	if( t->leaf) {
 		return 1;
 	}
-	//count += 1;
+	count += 1;
 	for( i = 0; i < t->degree; i++ ) {
-		count += 1 + _lrx_count_nodes( t->children[i] );
+		count +=  _lrx_count_nodes( t->children[i] );
 	}
 	return count;
 }
